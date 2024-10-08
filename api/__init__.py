e6a549a6478846bd6cd0f13cc369dc1e15586acc
Jason Li <realseasky@hotmail.com>
2024-10-08 11:05:04 +1100

    init: PPX code clean and components version update

---
+++ api/__init__.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+'''
+Author: 潘高
+LastEditors: 潘高
+Date: 2022-03-21 17:01:39
+LastEditTime: 2024-09-08 20:28:48
+Description: 业务层API，供前端JS调用
+usage: 在Javascript中调用window.pywebview.api.<methodname>(<parameters>)
+'''
+
+from api.storage import Storage
+from api.system import System
+from api.service import Service
+
+class API(System, Storage, Service):
+  '''业务层API，供前端JS调用'''
+
+  def setWindow(self, window):
+    '''获取窗口实例'''
+    System._window = window
---
+++ api/service/__init__.py
@@ -0,0 +1,23 @@
+import asyncio, aiohttp
+
+class Service(object):
+  def __init__(self):
+    pass
+  
+  async def _test(self):
+    print("Begain async test...")
+    for i in range(10):
+      await asyncio.sleep(1)
+      print(f"No.{i+1} tick")
+    print('test end...')
+    
+    async with aiohttp.ClientSession(connector=aiohttp.TCPConnector(ssl=False)) as session: #CS0035
+      async with session.get(url="https://www.google.com") as resp:
+        res = await resp.text()
+        print(res)
+    
+    return 'Hi Five'
+    
+    
+  def aiotest(self):
+    return asyncio.run(self._test())
